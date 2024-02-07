412934f6297b2ccd0331afa2610b880bdce196c4
Aaron Mahlke <aaron.mahlke@gmail.com>
2024-02-07 23:12:19 +0100

    Add animated hover

--- frontend/src/components/d-sidebar/d-sidebar-link.vue
+++ frontend/src/components/d-sidebar/d-sidebar-link.vue
@@ -4,12 +4,16 @@
     :to="to"
     :href="href"
     :target="href ? '_blank' : ''"
-    class="flex items-center h-9 gap-3 rounded-md pl-3 pr-1 text-neutral-500 transition-all duration-100 border border-transparent hover:bg-black/5 hover:text-neutral-950"
+    class="flex items-center h-9 gap-3 rounded-xl pl-3 pr-1 text-neutral-500 transition-all duration-100 border border-transparent hover:text-neutral-950 relative group"
     active-class=""
-    :class="{ '!bg-strong text-strong': active }"
+    :class="{ 'text-strong': active }"
   >
-    <component :is="icon" class="stroke-neutral-500" :size="18" :class="{ '!stroke-neutral-900': active }" />
-    <div class="text-sm w-full flex-1">
+    <div
+      class="absolute w-full h-full bg-black/5 left-0 rounded-xl scale-50 group-hover:scale-100 transition-all opacity-0 group-hover:opacity-100"
+      :class="{ '!bg-strong !scale-100 !opacity-100': active }"
+    ></div>
+    <component :is="icon" class="stroke-neutral-500 z-10" :size="18" :class="{ '!stroke-neutral-900': active }" />
+    <div class="text-sm w-full flex-1 z-10">
       <slot />
     </div>
   </component>
